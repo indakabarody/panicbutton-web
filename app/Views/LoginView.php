49f6a2ad6018dadad02fb7490861e407cc0a7401
Indaka Barody <indakabarody16@gmail.com>
2020-04-18 08:37:21 +0700

    Fixed login view

--- app/Views/LoginView.php
+++ app/Views/LoginView.php
@@ -21,7 +21,7 @@
 	<body class="hold-transition login-page">
 		<div class="login-box">
 			<div class="login-logo">
-				<a href="<?php echo base_url(); ?>/assets/index2.html"><b>DKT</b> PANIC BUTTON</a>
+				<a href="<?php echo base_url(); ?>/login"><b>DKT</b> Panic Button</a>
 			</div>
 			<!-- /.login-logo -->
 			<div class="card">
